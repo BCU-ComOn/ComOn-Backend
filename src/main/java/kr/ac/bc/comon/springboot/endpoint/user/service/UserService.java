c1cc6fde6fa9810d957e2277630df8e597740642
jueun275 <52851721+jueun275@users.noreply.github.com>
2021-01-17 21:47:26 +0900

    Create UserService.java

---
+++ src/main/java/kr/ac/bc/comon/springboot/endpoint/user/service/UserService.java
@@ -0,0 +1,10 @@
+package kr.ac.bc.comon.springboot.endpoint.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+}
