390b6b24efec5c70576a86baf44cfae3352b9250
jueun275 <52851721+jueun275@users.noreply.github.com>
2021-01-17 16:47:32 +0900

    Create BaseTime.java

---
+++ src/main/java/kr/ac/bc/comon/springboot/common/domain/BaseTime.java
@@ -0,0 +1,27 @@
+package kr.ac.bc.comon.springboot.common.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+
+    @CreatedDate
+    @Column(name = "CREATED_DT")
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "MODIFIED_DT")
+    private LocalDateTime modifiedDate;
+}
