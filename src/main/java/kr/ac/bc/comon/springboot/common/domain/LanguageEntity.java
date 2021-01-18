b887540f58f65decebf87c64f3e8bf825056938c
김진홍 <deepred.kim@gmail.com>
2021-01-18 14:28:10 +0900

    [BACK-44] 언어 Entity/Repository

---
+++ src/main/java/kr/ac/bc/comon/springboot/common/domain/LanguageEntity.java
@@ -0,0 +1,26 @@
+package kr.ac.bc.comon.springboot.common.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "LANGUAGE_TB")
+public class LanguageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "LANGUAGE_CD")
+    private Long languageCd;
+
+    @Column(name = "LANGUAGE_NM", nullable = false)
+    private String languageNm;
+
+    @Builder
+    public LanguageEntity(String languageNm) {
+        this.languageNm = languageNm;
+    }
+}
---
+++ src/main/java/kr/ac/bc/comon/springboot/common/repository/LanguageRepository.java
@@ -0,0 +1,9 @@
+package kr.ac.bc.comon.springboot.common.repository;
+
+import kr.ac.bc.comon.springboot.common.domain.LanguageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LanguageRepository extends JpaRepository<LanguageEntity, Long> {
+}
