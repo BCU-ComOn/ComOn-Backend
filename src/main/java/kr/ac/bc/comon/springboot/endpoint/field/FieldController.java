e16acf5643b7a79f737c5985e29964d95f3fb055
김진홍 <deepred.kim@gmail.com>
2021-01-18 15:36:06 +0900

    [BACK-49] 분야 Read DTO/Service/Controller

---
+++ src/main/java/kr/ac/bc/comon/springboot/endpoint/field/FieldController.java
@@ -0,0 +1,27 @@
+package kr.ac.bc.comon.springboot.endpoint.field;
+
+import kr.ac.bc.comon.springboot.endpoint.field.dto.FieldResponseDto;
+import kr.ac.bc.comon.springboot.endpoint.field.service.FieldService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+@RequestMapping("/field")
+@RequiredArgsConstructor
+@RestController
+public class FieldController {
+    private final FieldService fieldService;
+
+    @GetMapping("/{id}")
+    public FieldResponseDto findById(@PathVariable Long id){
+        return fieldService.findById(id);
+    }
+
+    @GetMapping("/list")
+    public List<FieldResponseDto> findAll() {
+        return fieldService.findAll();
+    }
+}
---
+++ src/main/java/kr/ac/bc/comon/springboot/endpoint/field/service/FieldService.java
@@ -0,0 +1,31 @@
+package kr.ac.bc.comon.springboot.endpoint.field.service;
+
+import kr.ac.bc.comon.springboot.common.domain.FieldEntity;
+import kr.ac.bc.comon.springboot.common.repository.FieldRepository;
+import kr.ac.bc.comon.springboot.endpoint.field.dto.FieldResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class FieldService {
+    private final FieldRepository fieldRepository;
+
+    @Transactional(readOnly = true)
+    public FieldResponseDto findById(Long id) {
+        FieldEntity fieldEntity = fieldRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 분야가 없습니다. id=" + id));
+
+        return new FieldResponseDto(fieldEntity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<FieldResponseDto> findAll() {
+        return fieldRepository.findAll().stream()
+                .map(FieldResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
