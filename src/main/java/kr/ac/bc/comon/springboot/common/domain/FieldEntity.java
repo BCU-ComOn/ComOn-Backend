591c30d4cc81038be994672226f2599bf6d3e4b7
김진홍 <deepred.kim@gmail.com>
2021-01-18 14:24:49 +0900

    BACK-43 분야 Entity/Repository 완성 및 기타
    @Repository 어노테이션 빠진 곳도 추가해줬음

---
+++ src/main/java/kr/ac/bc/comon/springboot/common/domain/FieldEntity.java
@@ -0,0 +1,26 @@
+package kr.ac.bc.comon.springboot.common.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "FIELD_TB")
+public class FieldEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "FIELD_CD")
+    private Long fieldCd;
+
+    @Column(name = "FIELD_NM", nullable = false)
+    private String fieldNm;
+
+    @Builder
+    public FieldEntity(String fieldNm) {
+        this.fieldNm = fieldNm;
+    }
+}
--- src/main/java/kr/ac/bc/comon/springboot/common/repository/FieldRepository.java
+++ src/main/java/kr/ac/bc/comon/springboot/common/repository/FieldRepository.java
@@ -1,10 +1,10 @@
 package kr.ac.bc.comon.springboot.common.repository;
 
-import kr.ac.bc.comon.springboot.common.domain.UserEntity;
+import kr.ac.bc.comon.springboot.common.domain.FieldEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface FieldRepository extends JpaRepository<FieldEntity, Long> {
 
 }
