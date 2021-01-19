a9782238ee5b66190cc88982dcdf633cc0e36f22
wsb0722 <wsb0722@naver.com>
2021-01-19 11:50:28 +0900

    [BACK-62] Create ProjectLanguage Repository

---
+++ src/main/java/kr/ac/bc/comon/springboot/common/repository/ProjectLanguageRepository.java
@@ -0,0 +1,16 @@
+package kr.ac.bc.comon.springboot.common.repository;
+
+import kr.ac.bc.comon.springboot.common.domain.LanguageEntity;
+import kr.ac.bc.comon.springboot.common.domain.ProjectEntity;
+import kr.ac.bc.comon.springboot.common.domain.ProjectLanguageEntity;
+import kr.ac.bc.comon.springboot.common.pk.ProjectLanguagePK;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProjectLanguageRepository extends JpaRepository<ProjectLanguageEntity, ProjectLanguagePK> {
+    ProjectLanguageEntity findByProjectFkAndLanguageFkNm(ProjectEntity projectFk, LanguageEntity languageFkNm);
+    List<ProjectEntity> findAllByProjectFk(ProjectEntity projectFk);
+}
