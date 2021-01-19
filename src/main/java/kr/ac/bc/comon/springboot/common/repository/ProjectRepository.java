e174d864f58723be1dbe472f06cedc5439139019
wsb0722 <wsb0722@naver.com>
2021-01-19 11:55:19 +0900

    [BACK-61] Update Project Repository

--- src/main/java/kr/ac/bc/comon/springboot/common/repository/ProjectRepository.java
+++ src/main/java/kr/ac/bc/comon/springboot/common/repository/ProjectRepository.java
@@ -4,6 +4,11 @@ import kr.ac.bc.comon.springboot.common.domain.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+    ProjectEntity findByProjectCd(Long projectCd);
+    List<ProjectEntity> findByProjectProgress(Integer projectProgress);
+    List<ProjectEntity> findByProjectGenerationNum(Integer projectGenerationNum);
 }
